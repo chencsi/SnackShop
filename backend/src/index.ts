871bb7689642fceee4920e73a808c1b4d5f77ce9
Chencsi <kevinkeeeh@gmail.com>
2025-07-22 23:39:02 +0200

    refactor server initialization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,27 @@
-import Fastify from 'fastify';
-import fastifyJwt from '@fastify/jwt';
+import Fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
 
 import userRoutes from './routes/user';
 import productRoutes from './routes/product';
 import orderRoutes from './routes/order';
 
 const fastify = Fastify({ logger: true });
-fastify.register(fastifyJwt, { secret: 'snackshopsecretkey' });
+
+fastify.get("/", async () => {
+	return { message: "Server is running" };
+});
+
+fastify.register(fastifyJwt, { secret: 'supersecretkey' });
 
 userRoutes(fastify);
 productRoutes(fastify);
 orderRoutes(fastify);
 
-fastify.listen({ port: 3001, host: '0.0.0.0' });
+(async () => {
+	try {
+		await fastify.listen({ port: 3001, host: "0.0.0.0" });
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+})();
