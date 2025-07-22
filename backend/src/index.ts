388b28404eefff240414ee0644207b76f8142d2c
Chencsi <kevinkeeeh@gmail.com>
2025-07-22 21:38:46 +0200

    use routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,15 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
+import fastifyJwt from '@fastify/jwt';
+
+import userRoutes from './routes/user';
+import productRoutes from './routes/product';
+import orderRoutes from './routes/order';
 
 const fastify = Fastify({ logger: true });
+fastify.register(fastifyJwt, { secret: 'snackshopsecretkey' });
 
-fastify.get("/", async () => {
-	return { message: "Server is running" };
-});
+userRoutes(fastify);
+productRoutes(fastify);
+orderRoutes(fastify);
 
-const start = async () => {
-	try {
-		await fastify.listen({ port: 3001, host: "0.0.0.0" });
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
-};
-
-start();
+fastify.listen({ port: 3001, host: '0.0.0.0' });
