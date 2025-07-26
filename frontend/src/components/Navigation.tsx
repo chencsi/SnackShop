f1f2897cc2e1aee9c8e69b420f33c84b893665bb
Chencsi <kevinkeeeh@gmail.com>
2025-07-26 10:56:11 +0200

    basic navigation bar created

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,18 +1,15 @@
-import { Box, Flex, Link as ChakraLink, Button, Spacer } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 
 export default function Navigation() {
     return (
-        <nav className="bg-amber-300 flex items-center">
-                <ChakraLink as={RouterLink} to="/" fontWeight="bold" fontSize="lg" color="teal.500">
-                    SnackShop
-                </ChakraLink>
-                <Spacer />
-                <div className='flex flex-row gap-4'>
-                    <ChakraLink as={RouterLink} to="/login" color="teal.600" _hover={{ textDecoration: 'underline' }}>
-                        <Button variant={'outline'} bg={'white'}>Login</Button>
-                    </ChakraLink>
-                </div>
+        <nav className="fixed top-0 left-0 w-[calc(100%-1rem)] z-50 p-5 mx-2 mt-2 flex bg-white rounded-lg shadow-lg text-stone-700">
+            <RouterLink
+            to="/"
+            className="font-extrabold text-2xl drop-shadow-2xl"
+            >
+            Snack Shop
+            </RouterLink>
+            {}
         </nav>
     )
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="py-5">
+        <div>
             <h1 className="text-xl font-bold">Home Page</h1>
         </div>
     )
